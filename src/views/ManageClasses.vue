5438fe7d7e585345aad447897e3ab93a5eab7e18
kashu-02 <hituji_shun@icloud.com>
2021-05-10 00:03:57 +0900

    complete class page

---
+++ src/views/ManageClasses.vue
@@ -0,0 +1,207 @@
+<template>
+  <div class="login">
+    <global-header />
+    <v-main>
+      <v-alert
+        color="red"
+        dense
+        dismissible
+        outlined
+        text
+        type="error"
+        v-if="alert"
+      >
+        {{ alertmessage }}  
+      </v-alert>
+      <v-card>
+        <v-data-table
+          :headers="headers"
+          :items="classes"
+          :items-per-page="15"
+          class="elevation-1"
+          :loading="loading"
+          loading-text="Loading... Please wait"
+        >
+          <template v-slot:[`item.isRegistered`]="{ item }">
+            <v-btn
+              small
+              color="light-green"
+              @click.stop="Unsubscribe(item)"
+              v-if="item.isRegistered"
+              :loading="loading"
+            >
+              登録解除
+            </v-btn>
+            <v-btn
+              small
+              color="amber"
+              @click.stop="Subscribe(item)"
+              v-if="!item.isRegistered"
+              :loading="loading"
+            >
+              登録
+            </v-btn>
+          </template>
+        </v-data-table>
+      </v-card>
+    </v-main> 
+  </div>
+</template>
+
+<script>
+import GlobalHeader from "../components/GlobalHeader.vue";
+
+export default {
+  components: {
+    GlobalHeader,
+  },
+  data: () => ({
+    loading: true,
+    alert: false,
+    alertmessage: "",
+    headers:[
+      { text: 'ClassId', value: 'id' },
+      {text: 'クラス名', value: 'name'},
+      { text: '登録/登録解除', value: 'isRegistered' }
+    ]
+  }),
+  mounted: async function(){
+    try {
+      let classes = await this.axios.get(`${process.env.VUE_APP_API_BASE_URL}/getclasses`,
+        {
+        headers: {
+          Authorization: `Bearer ${this.$store.state.jwt_token}`,
+        },
+      })
+      let sendlist = await this.axios.get(`${process.env.VUE_APP_API_BASE_URL}/sendlist`,
+        {
+        headers: {
+          Authorization: `Bearer ${this.$store.state.jwt_token}`,
+        },
+      })
+
+      this.classes = classes.data.map( function(value){
+        const found = sendlist.data.find(element => element.ClassId == value.id)
+        if(found){
+          value.isRegistered = true
+        }else{
+          value.isRegistered = false
+        }
+        return value
+      }) 
+      console.log(this.classes)
+      this.loading = false 
+    } catch (error) {
+      console.error(error)
+      this.alert = true
+      this.alertmessage = error.message || 'エラーが発生しました。しばらくしてから再読み込みをしてください。'
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+      return null
+    }
+  },
+  methods: {
+    async Subscribe(item){
+      this.loading = true
+      try {
+        const result = await this.axios.post(`${process.env.VUE_APP_API_BASE_URL}/sendlist`,
+        {
+          ClassId: item.id 
+        },
+          {
+          headers: {
+            Authorization: `Bearer ${this.$store.state.jwt_token}`,
+          },
+        })
+        if(result.status == 200){
+          let classes = await this.axios.get(`${process.env.VUE_APP_API_BASE_URL}/getclasses`,
+            {
+            headers: {
+              Authorization: `Bearer ${this.$store.state.jwt_token}`,
+            },
+          })
+          let sendlist = await this.axios.get(`${process.env.VUE_APP_API_BASE_URL}/sendlist`,
+            {
+            headers: {
+              Authorization: `Bearer ${this.$store.state.jwt_token}`,
+            },
+          })
+
+          this.classes = classes.data.map( function(value){
+            const found = sendlist.data.find(element => element.ClassId == value.id)
+            if(found){
+              value.isRegistered = true
+            }else{
+              value.isRegistered = false
+            }
+            return value
+          }) 
+          console.log(this.classes)
+          this.loading = false 
+        }
+        
+      } catch (error) {
+        console.error(error)
+        this.alert = true
+        this.alertmessage = error.message || 'エラーが発生しました。しばらくしてから再読み込みをしてください。'
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth"
+        })
+        return null
+      }
+
+    },
+    async Unsubscribe(item){
+      this.loading = true
+      try {
+        const result = await this.axios.delete(`${process.env.VUE_APP_API_BASE_URL}/sendlist?ClassId=${item.id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${this.$store.state.jwt_token}`,
+          },
+        })
+        if(result.status == 200){
+          let classes = await this.axios.get(`${process.env.VUE_APP_API_BASE_URL}/getclasses`,
+            {
+            headers: {
+              Authorization: `Bearer ${this.$store.state.jwt_token}`,
+            },
+          })
+          let sendlist = await this.axios.get(`${process.env.VUE_APP_API_BASE_URL}/sendlist`,
+            {
+            headers: {
+              Authorization: `Bearer ${this.$store.state.jwt_token}`,
+            },
+          })
+
+          this.classes = classes.data.map( function(value){
+            const found = sendlist.data.find(element => element.ClassId == value.id)
+            if(found){
+              value.isRegistered = true
+            }else{
+              value.isRegistered = false
+            }
+            return value
+          }) 
+          console.log(this.classes)
+          this.loading = false 
+        }
+        
+      } catch (error) {
+        console.error(error)
+        this.alert = true
+        this.alertmessage = error.message || 'エラーが発生しました。しばらくしてから再読み込みをしてください。'
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth"
+        })
+        return null
+      }
+
+    }
+  }
+}
+</script>
---
+++ src/views/Settings.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="setings">
+    <global-header />
+    <v-main>
+      <v-alert
+        color="red"
+        dense
+        dismissible
+        outlined
+        text
+        type="error"
+        v-if="alert"
+      >
+        {{ alertmessage }}  
+      </v-alert>
+      <v-card class="ma-3">
+        <v-card-title class="ma-5">
+          登録情報の削除
+        </v-card-title>
+        <v-container>
+          <p>Classroom-Notifierへ登録されているアカウントを削除します。</p>
+            <v-row justify="center">
+              <v-col class="text-center">
+                <v-btn
+                  color="red"
+                  elevation="2"
+                  outlined
+                >
+                  アカウントを削除
+                </v-btn>
+              </v-col>
+            </v-row>
+        </v-container>
+      </v-card>
+    </v-main> 
+  </div>
+</template>
+
+<script>
+import GlobalHeader from "../components/GlobalHeader.vue";
+
+export default {
+  components: {
+    GlobalHeader,
+  },
+  data: () => ({
+    alert: false,
+    alertmessage: "",
+  }),
+  methods: {
+
+  }
+}
+</script>
