3d5afa0c455a2fc2c7b1a8b4aa1c2b1e63fbcefe
kashu-02 <hituji_shun@icloud.com>
2021-07-05 23:52:55 +0900

    change title

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   "pages": {
     "index": {
       "entry": "src/main.js",
-      "title": "Clasroom-Notifier"
+      "title": "Clasroom Notifier"
     }
   },
   "transpileDependencies": [
